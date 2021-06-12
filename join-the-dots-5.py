7d603ea67f1ca537521c6a13a3675880bbdc2f49
sumitdhakad2837 <sumitdhakad2232@gmail.com>
2021-06-12 20:53:33 +0530

    added last video

--- join-the-dots-5.py
+++ join-the-dots-5.py
@@ -16,12 +16,47 @@ font72 = pygame.font.SysFont(None,72)
 font64 = pygame.font.SysFont(None,64)
 font56 = pygame.font.SysFont(None,56)
 
+def res_page(player):
+    res = True
+    
+    while res:
+        screen.fill((170,170,170))
+        
+        player_text = font72.render('player '+str(player)+' won the match.',True,(0,0,0))
+        player_text_rect = player_text.get_rect()
+        player_text_rect.center = (width/2,height/3)
+        screen.blit(player_text,player_text_rect)
+        
+        rect = pygame.draw.rect(screen,(255,255,255),(width/2-100,height/2-50,200,100),10)
+        back_text = font56.render(' back',True,(255,255,255))
+        back_text_rect = back_text.get_rect()
+        back_text_rect.center = rect.center
+        screen.blit(back_text,back_text_rect)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                res = False
+                
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.pos[0]>rect.x and event.pos[1]>rect.y and event.pos[0]<(rect.x+rect.width) and event.pos[1]<(rect.y+rect.height):
+                    res = False       
+        
+        pygame.display.update()
+        pygame.display.flip()
+        Clock.tick(30)
+        
+
+
 def main_screen(no_of_dots):
     
     main = True
     player1_score = 0
     player2_score = 0
     
+    player = 1
+    
+    player_name_arr = []
+        
     dots_arr = []
     mouse_pos = []
     line_arr = []
@@ -53,6 +88,18 @@ def main_screen(no_of_dots):
         dots_text_rect.center = (width/2,100)
         screen.blit(dots_text,dots_text_rect)
         
+        if player==1:
+            pygame.draw.circle(screen,(155,0,0),(400,100),20)
+        else:
+            pygame.draw.circle(screen,(0,0,255),(width-400,100),20)
+        
+        for sub_arr in player_name_arr:
+            name_text = font72.render('P'+str(sub_arr[2]),True,(255,255,255))
+            name_text_rect = name_text.get_rect()
+            name_text_rect.center = (sub_arr[0],sub_arr[1])
+            screen.blit(name_text,name_text_rect)
+                    
+                
         for in_arr in dots_arr:
             for cord in in_arr:
                 pygame.draw.circle(screen,(0,0,0),cord,5)
@@ -76,6 +123,16 @@ def main_screen(no_of_dots):
         
         for cord_arr in line_arr:
             pygame.draw.line(screen,(0,0,0),cord_arr[0],cord_arr[1],5)
+        
+        if len(line_arr)==(2*no_of_dots*(no_of_dots-1)):
+            if player1_score>player2_score:
+                res_page(1)
+            elif  player1_score==player2_score:
+                res_page(0)
+            else:
+                res_page(2)
+            main=False
+                
          
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -87,7 +144,31 @@ def main_screen(no_of_dots):
                         cord = dots_arr[i][j]
                         hr_next_cord = dots_arr[i][j+1]
                         
-                        if event.pos[0]>cord[0] and event.pos[0]<hr_next_cord[0] and event.pos[1]>cord[1]-30 and event.pos[1]<cord[1]+30 :
+                        if event.pos[0]>cord[0] and event.pos[0]<hr_next_cord[0] and event.pos[1]>cord[1]-30 and event.pos[1]<cord[1]+30 and [ cord, hr_next_cord ] not in line_arr:
+                            
+                            change_player = True
+                            
+                            if i!=(no_of_dots-1) and [dots_arr[i+1][j],dots_arr[i+1][j+1]] in line_arr and [dots_arr[i][j],dots_arr[i+1][j]] in line_arr and [dots_arr[i][j+1],dots_arr[i+1][j+1]] in line_arr:
+                                player_name_arr.append([cord[0]+line_width/2, cord[1]+line_height/2,player])
+                                change_player = False
+                                if player==1:
+                                    player1_score += 1
+                                else:
+                                    player2_score += 1
+                                
+                            if i!=0 and [dots_arr[i-1][j],dots_arr[i-1][j+1]] in line_arr and [dots_arr[i-1][j],dots_arr[i][j]] in line_arr and [dots_arr[i-1][j+1],dots_arr[i][j+1]] in line_arr:
+                                player_name_arr.append([cord[0]+line_width/2, cord[1]-line_height/2,player])
+                                change_player = False
+                                if player==1:
+                                    player1_score += 1
+                                else:
+                                    player2_score += 1
+                                
+                            if player==1 and change_player:
+                                player = 2
+                            elif change_player:
+                                player = 1
+                                                        
                             line_arr.append([cord,hr_next_cord])
            
                 for i in range(no_of_dots-1):
@@ -95,7 +176,34 @@ def main_screen(no_of_dots):
                        cord = dots_arr[i][j]
                        vr_next_cord = dots_arr[i+1][j]
                        
-                       if event.pos[1]>cord[1] and event.pos[1]<vr_next_cord[1] and event.pos[0]>cord[0]-30 and event.pos[0]<cord[0]+30 :
+                       if event.pos[1]>cord[1] and event.pos[1]<vr_next_cord[1] and event.pos[0]>cord[0]-30 and event.pos[0]<cord[0]+30 and [ cord, vr_next_cord ] not in line_arr:
+                           
+                           
+                           change_player = True
+                            
+                           if j!=(no_of_dots-1) and [dots_arr[i][j+1],dots_arr[i+1][j+1]] in line_arr and [dots_arr[i][j],dots_arr[i][j+1]] in line_arr and [dots_arr[i+1][j],dots_arr[i+1][j+1]] in line_arr:
+                               player_name_arr.append([cord[0]+line_width/2, cord[1]+line_height/2,player])
+                               change_player = False
+                               if player==1:
+                                    player1_score += 1
+                               else:
+                                    player2_score += 1
+                                
+                                
+                           if j!=0 and [dots_arr[i][j-1],dots_arr[i+1][j-1]] in line_arr and [dots_arr[i][j-1],dots_arr[i][j]] in line_arr and [dots_arr[i+1][j-1],dots_arr[i+1][j]] in line_arr:
+                               player_name_arr.append([cord[0]-line_width/2, cord[1]+line_height/2,player])
+                               change_player = False
+                               if player==1:
+                                    player1_score += 1
+                               else:
+                                    player2_score += 1
+                                
+                            
+                           if player==1 and change_player:
+                               player = 2
+                           elif change_player:
+                               player = 1
+                          
                            line_arr.append([cord,vr_next_cord])
                
                 
